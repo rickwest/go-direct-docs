c3b986f9d02eca8d5ab86d2267e2da4b09b226e4
rick <rickwestdev@gmail.com>
2019-04-11 22:49:23 +0100

    try hard-coding logo path

--- conf.py
+++ conf.py
@@ -88,6 +88,8 @@ html_theme_options = {
     'github_button': 'true'
 }
 
+html_logo = '_static/logo.png'
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
