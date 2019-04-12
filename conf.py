9402d2ed3ccd2a269e79dcaf64626fe78d2b7fce
rick <rickwestdev@gmail.com>
2019-04-12 23:38:33 +0100

    Remove last few references to gd

--- conf.py
+++ conf.py
@@ -114,7 +114,7 @@ html_sidebars = {
 # -- Options for HTMLHelp output ---------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'GoDirectdoc'
+htmlhelp_basename = 'Shuutdoc'
 
 
 # -- Options for LaTeX output ------------------------------------------------
@@ -151,7 +151,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'godirect', 'Shuut Documentation',
+    (master_doc, 'Shuut', 'Shuut Documentation',
      [author], 1)
 ]
 
@@ -162,8 +162,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'GoDirect', 'Shuut Documentation',
-     author, 'GoDirect', 'One line description of project.',
+    (master_doc, 'Shuut', 'Shuut Documentation',
+     author, 'Shuut', 'One line description of project.',
      'Miscellaneous'),
 ]
 
